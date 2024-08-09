2f9e1d3e7fd5fbbaebad8b24bfffe85b9852bf29
Rupinder Sidhu <rsidhu10@gmail.com>
2024-08-09 12:14:10 +0530

    add todo using redux

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{html,js,jsx}"],
+  //content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
